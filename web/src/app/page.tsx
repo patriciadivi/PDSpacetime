bdadc6b3c993f16915a396c0f1c7829b749ccc0b
PatriciaDivi <patriciavianad@gmail.com>
2023-05-17 03:45:36 -0300

    chore(Development): starting spacetime project | WEB config setup :rocket:

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950 ">
+      <h1 className="bg-violet-700 p-6 text-center text-4xl font-bold text-zinc-50 ">
+        Hello Word
+      </h1>
+    </div>
+  )
+}
